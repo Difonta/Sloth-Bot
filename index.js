c8d096764dd1d89824fe32e7ff3e8f1470bedd73
daniel zsa <user@DESKTOP-12475.12475>
2022-03-21 17:45:13 +0800

    commit message

---
+++ index.js
@@ -0,0 +1,25 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+// Create a new client instance
+const client = new Client({ intents:  [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+// When the client is ready, run this code (only once)
+client.on('ready', () => {
+    console.log('Ready!');
+    
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong'
+        })
+    }
+});
+
+
+    
+// Login to Discord with your client's token
+client.login(process.env.TOKEN);
